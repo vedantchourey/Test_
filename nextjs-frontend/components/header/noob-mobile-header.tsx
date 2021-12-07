c64c59fff9dac8e495bd9c320302089602521487
piggy <babymechanic@gmail.com>
2021-12-07 16:25:47 +1100

    piggy | added the drawer for mobile view

--- nextjs-frontend/components/header/noob-mobile-header.tsx
+++ nextjs-frontend/components/header/noob-mobile-header.tsx
@@ -3,25 +3,35 @@ import styles from './noob-mobile-header.module.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import Image from 'next/image';
 import * as React from 'react';
+import { useState } from 'react';
+import NoobDrawer from '../drawer/noob-drawer';
 
 export default function NoobMobileHeader() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleDrawer = () => setShowMenu(true);
+  const hideMenu = () => setShowMenu(false);
+
   return (
-    <AppBar position="fixed" className={styles.appBar}>
-      <div className={styles.topHeader}>
-        <div className={styles.appBarItem}>
+    <>
+      <AppBar position="fixed" className={styles.appBar}>
+        <div className={styles.topHeader}>
+          <div className={styles.appBarItem}>
+          </div>
+          <div className={styles.appBarItemMiddle}>
+            <Image src="/images/noobstorm-logo-small.png"
+                   width={130}
+                   height={28}
+                   alt="noob storm logo"/>
+          </div>
+          <div className={styles.appBarItemLast}>
+            <IconButton onClick={toggleDrawer}>
+              <MenuIcon/>
+            </IconButton>
+          </div>
         </div>
-        <div className={styles.appBarItemMiddle}>
-          <Image src="/images/noobstorm-logo-small.png"
-                 width={130}
-                 height={28}
-                 alt="noob storm logo"/>
-        </div>
-        <div className={styles.appBarItemLast}>
-          <IconButton>
-            <MenuIcon/>
-          </IconButton>
-        </div>
-      </div>
-    </AppBar>
+      </AppBar>
+      <NoobDrawer show={showMenu} onClose={hideMenu}/>
+    </>
   );
 }
---
+++ nextjs-frontend/components/icons/twitch-icon.tsx
@@ -0,0 +1,13 @@
+import { SvgIcon } from '@mui/material';
+import * as React from 'react';
+
+export default function TwitchIcon() {
+  return (
+    <SvgIcon>
+      <path
+        d="M2.37969 0.5L0.96875 4.10469V18.8375H5.98438V21.5H8.80625L11.4688 18.8375H15.5469L21.0312 13.3531V0.5H2.37969ZM19.1516 12.4109L16.0156 15.5469H11L8.3375 18.2094V15.5469H4.10469V2.37969H19.1516V12.4109ZM16.0156 5.98438V11.4641H14.1359V5.98438H16.0156ZM11 5.98438V11.4641H9.12031V5.98438H11Z"
+        fill="white"/>
+    </SvgIcon>
+  );
+
+}
