9dfb3f2dc714d085f947c1da6e01b2e8e3807d57
piggy <babymechanic@gmail.com>
2021-12-16 14:27:13 +1100

    fixing styling for mobile view

--- nextjs-frontend/components/cards/user-profile-pic/user-profile-pic.tsx
+++ nextjs-frontend/components/cards/user-profile-pic/user-profile-pic.tsx
@@ -11,14 +11,18 @@ import { v4 } from 'uuid';
 import { updateProfileImages } from '../../../services/front-end-services/profile-service';
 import { setUserProfile } from '../../../store/authentication/authentication-slice';
 import { ProfileImageTypes } from '../../../services/backend-services/profile-service/profile-image-types';
+import { isDeviceTypeSelector } from '../../../store/layout/layout-selectors';
+import { deviceTypes } from '../../../store/layout/device-types';
 
 
 export default function UserProfilePic() {
   const [avatarUrl, setAvatarUrl] = useState<string | undefined>();
   const [backgroundUrl, setBackgroundUrl] = useState<string | undefined>();
   const userProfile = useAppSelector(userProfileSelector);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const backgroundInputRef = useRef<HTMLInputElement>(null);
+  const avatarInputRef = useRef<HTMLInputElement>(null);
   const appDispatch = useAppDispatch();
+  const isDesktop = useAppSelector(x => isDeviceTypeSelector(x, deviceTypes.desktop));
 
   useEffect(() => {
     (async () => {
@@ -39,51 +43,68 @@ export default function UserProfilePic() {
       const objectURL: string = URL.createObjectURL(usersBackground.data);
       setBackgroundUrl(objectURL);
     })();
-
   }, [userProfile])
 
   function showUploadBackgroundPicker() {
-    fileInputRef.current?.click();
+    backgroundInputRef.current?.click();
   }
 
-  function generateFileName(file: File) {
+  function generateFileUrl(type: 'avatar' | 'avatarBackground', file: File) {
     const fileExt = file.name.split('.').pop()?.toLowerCase();
-    return `${userProfile!.id}${v4()}.${fileExt}`;
+    return `avatars/${type}${userProfile!.id}${v4()}.${fileExt}`;
   }
 
-  async function updateUserProfile(fileUrl: string, file: File) {
-    const response = await updateProfileImages({url: fileUrl, imageType: ProfileImageTypes.background});
+  async function updateUserProfile(fileUrl: string, file: File, imageType: ProfileImageTypes) {
+    const response = await updateProfileImages({url: fileUrl, imageType: imageType});
     if (response.isError) return console.error(response);
     appDispatch(setUserProfile(response));
-    setBackgroundUrl(URL.createObjectURL(file));
+    if (imageType === ProfileImageTypes.avatar) {
+      setAvatarUrl(URL.createObjectURL(file));
+    } else {
+      setBackgroundUrl(URL.createObjectURL(file));
+    }
   }
 
-  async function onUploadBackground(event: ChangeEvent<HTMLInputElement>) {
+  async function onUploadAvatar(event: ChangeEvent<HTMLInputElement>) {
     if (!event.target.files || event.target.files.length == 0) return;
+    const file = event.target.files[0];
+    await uploadImageAndLinkToProfile('avatar', file, ProfileImageTypes.avatar);
+  }
+
+  async function uploadImageAndLinkToProfile(imageNamePrefix: 'avatar' | 'avatarBackground', file: File, imageType: ProfileImageTypes) {
     appDispatch(setIsLoading(true));
     try {
-      const file = event.target.files[0];
-      const fileName = generateFileName(file);
-      const fileUrl = `avatars/${fileName}`;
+      const fileUrl = generateFileUrl(imageNamePrefix, file);
       const uploadResult = await uploadImage('resources', fileUrl, file);
-      if (uploadResult.error == null) {
-        await updateUserProfile(fileUrl, file);
-      }
+      if (uploadResult.error == null) await updateUserProfile(fileUrl, file, imageType);
     } finally {
       appDispatch(setIsLoading(false));
     }
   }
 
+  async function onUploadBackground(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files || event.target.files.length == 0) return;
+    const file = event.target.files[0];
+    await uploadImageAndLinkToProfile('avatarBackground', file, ProfileImageTypes.background);
+  }
+
+  function showUploadAvatarPicker() {
+    avatarInputRef.current?.click();
+  }
+
   return (
-    <Card sx={{maxWidth: 440, borderRadius: 0, display: 'flex', flexDirection: 'column', backgroundColor: '#100626'}}>
+    <Card sx={{maxWidth: 440, borderRadius: 0, display: 'flex', flexDirection: 'column', backgroundColor: isDesktop ? '#100626' : '#08001C'}}>
       <div className={styles.imageBackgroundContainer}>
-        <Fab color="primary" aria-label="edit" className={styles.editBackgroundButton} onClick={showUploadBackgroundPicker}>
+        <Fab color="primary" aria-label="edit" size="small" className={styles.editBackgroundButton} onClick={showUploadBackgroundPicker}>
+          <AddPhotoAlternateOutlinedIcon/>
+        </Fab>
+        <Fab color="primary" aria-label="edit" size="small" className={styles.editAvatarButton} onClick={showUploadAvatarPicker}>
           <AddPhotoAlternateOutlinedIcon/>
         </Fab>
         <Avatar alt="Remy Sharp"
                 sx={{width: 156, height: 156}}
                 className={styles.userProfilePic}
-                src="/images/default-user-profile-background.jpg" />
+                src={avatarUrl || "/images/default-user-profile-background.jpg"}/>
         <div style={{overflow: 'hidden', flexGrow: 1}}>
           <img className={styles.imageBackground}
                src={backgroundUrl || "/images/default-user-profile-background.jpg"}
@@ -136,7 +157,8 @@ export default function UserProfilePic() {
           </div>
         </div>
       </div>
-      <input type="file" ref={fileInputRef} style={{display: 'none'}} onChange={onUploadBackground} accept=".jpeg,.jpg,.png"/>
+      <input type="file" ref={backgroundInputRef} style={{display: 'none'}} onChange={onUploadBackground} accept=".jpeg,.jpg,.png"/>
+      <input type="file" ref={avatarInputRef} style={{display: 'none'}} onChange={onUploadAvatar} accept=".jpeg,.jpg,.png"/>
     </Card>
   )
 }
