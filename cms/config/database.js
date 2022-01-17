860f0acd414183f6b3e0ccc1830ec68fefda9aeb
developeranku <ankur4736@gmail.com>
2022-01-18 00:02:26 +0530

    changed db name for cms

--- cms/config/database.js
+++ cms/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'db.bqungsamjrdpbmzwnoyz.supabase.co'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
+      database: env('DATABASE_NAME', 'noobstorm-cms'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'Noobstorm@123'),
       ssl: env.bool('DATABASE_SSL', false),
