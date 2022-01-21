37a9e4409b40de637f9a64144fc87a7e87ce09a3
developeranku <ankur4736@gmail.com>
2022-01-21 17:12:32 +0530

    added linting rules

--- nextjs-frontend/next.config.js
+++ nextjs-frontend/next.config.js
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack', 'url-loader'],
--- nextjs-frontend/pages/about-us/index.tsx
+++ nextjs-frontend/pages/about-us/index.tsx
@@ -1,25 +1,23 @@
 import Head from 'next/head';
 import commonStyles from '../../src/frontend/styles/common.module.css';
 import { Container, Grid, Typography, useTheme } from '@mui/material';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import styles from '../account.module.css';
-import { useAppSelector } from '../../src/frontend/redux-store/redux-store';
-import { authCheckStatusSelector, isLoggedInSelector } from '../../src/frontend/redux-store/authentication/authentication-selectors';
-import { getAppHeaderHeightSelector } from '../../src/frontend/redux-store/layout/layout-selectors';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import NoobHeader from '../../src/frontend/components/header/noob-header';
-import UserProfileCard from '../../src/frontend/components/cards/user-profile-card/user-profile-card';
 import Heading from '../../src/frontend/components/typography/heading';
-import NewsletterPoster from "../../src/frontend/components/newsletter-poster"
 
-const TEXT = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry.`
+interface IPageProps {
+    pageContent: {
+        attributes: {
+            title: string,
+            description: string
+        }
+    } | null
+}
 
-export default function AboutUs() {
+export default function AboutUs(props: IPageProps) {
+    const content = props.pageContent?.attributes;
     const theme = useTheme();
-    const router = useRouter();
-    const isLoggedIn = useAppSelector(isLoggedInSelector);
-    const checkStatus = useAppSelector(authCheckStatusSelector);
-    const appHeaderHeight = useAppSelector(getAppHeaderHeightSelector);
 
     // useEffect(() => {
     //     (async () => {
@@ -47,14 +45,12 @@ export default function AboutUs() {
                         <Grid item xs={12} lg={6}>
 
                             <Typography className={commonStyles.primaryText} marginBottom={2}>
-                                {TEXT.substring(0, 70)}
+                                {content?.title}
                             </Typography>
 
-                            {new Array(2).fill(TEXT).map((text, i) => (
-                                <Typography className={commonStyles.whiteText} marginBottom={2}>
-                                    {text}
-                                </Typography>
-                            ))}
+                            <Typography className={commonStyles.whiteText} marginBottom={2}>
+                                {content?.description}
+                            </Typography>
 
                         </Grid>
 
@@ -62,9 +58,9 @@ export default function AboutUs() {
                             <img src="/images/about-us-hero.png" className={commonStyles.fillImage} />
                         </Grid>
 
-                        <Grid item xs={12}>
+                        {/* <Grid item xs={12}>
                             <NewsletterPoster />
-                        </Grid>
+                        </Grid> */}
 
                     </Grid>
 
@@ -73,4 +69,38 @@ export default function AboutUs() {
             </main>
         </div>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const {
+        NEXT_PUBLIC_CMS_API_ENDPOINT,
+        NEXT_PUBLIC_CMS_API_TOKEN
+    } = process.env;
+    try {
+        const endpoint = NEXT_PUBLIC_CMS_API_ENDPOINT + '/api/about-us';
+        const res = await axios.get(endpoint, {
+            headers: {
+                'Authorization': 'Bearer ' + NEXT_PUBLIC_CMS_API_TOKEN
+            },
+            params: {
+                populate: '*'
+            }
+        });
+
+        return {
+            props: {
+                pageContent: res.data.data
+            }
+        }
+
+    }
+    catch (err) {
+        // console.log(err);
+        return {
+            props: {
+                pageContent: null
+            }
+        }
+    }
 }
