7fa7224e9613bea47caa605d4312453edf0289b7
lovekushtyagi <lovekush@octifytechnologies.com>
2022-01-28 20:54:43 +0530

    update comment

---
+++ nextjs-frontend/pages/api/update-comment.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createNextJsRouteHandler } from '../../src/backend/utils/api-middle-ware/api-handler-factory';
+import { PerRequestContext } from '../../src/backend/utils/api-middle-ware/api-middleware-typings';
+import { beginTransactionMiddleWare, commitOrRollBackTransactionMiddleWare } from '../../src/backend/utils/api-middle-ware/transaction-middle-ware';
+import { updateComment } from '../../src/backend/services/posts-services/update-comment/update-comment';
+import { authenticatedUserMiddleware } from '../../src/backend/utils/api-middle-ware/auth-middle-ware';
+
+
+export default createNextJsRouteHandler({
+    patch: {
+        handler: async (req: NextApiRequest, res: NextApiResponse, context: PerRequestContext) => {
+            const result = await updateComment(req.body, context);
+            res.status(result.errors ? 400 : 200).send(result);
+        },
+        preHooks: [authenticatedUserMiddleware, beginTransactionMiddleWare],
+        postHooks: [commitOrRollBackTransactionMiddleWare]
+    }
+})
--- nextjs-frontend/src/backend/services/database/repositories/post-comments-repository.ts
+++ nextjs-frontend/src/backend/services/database/repositories/post-comments-repository.ts
@@ -2,6 +2,11 @@ import { Knex } from 'knex';
 import { BaseRepository } from './base-repository';
 import { IPostComments } from '../models/i-post-comments';
 
+interface IUpdateComment {
+    comment?: string;
+    postId?: string;
+}
+
 export class PostCommentsRepository extends BaseRepository<IPostComments>{
     constructor(transaction: Knex.Transaction) {
         super(transaction, 'post_comments');
@@ -11,4 +16,17 @@ export class PostCommentsRepository extends BaseRepository<IPostComments>{
         return await this.entities().insert(comment);
     }
 
+    async getCommentById(id: string) {
+        return await this.entities().select('*').first().where('id', id)
+    }
+
+    async countCommentById(id: string): Promise<number> {
+        const result = await this.entities().whereRaw("id = ?", [id])
+                                 .count('id');
+        return parseInt(result[0].count, 10);
+      }
+
+    async updateComment(id: string, update: IUpdateComment): Promise<any> {
+        return await this.entities().where('id', id).update(update);
+    }
 }
