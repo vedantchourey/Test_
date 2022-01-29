7833cfcdac934643424b74d1f0fba3e36ea8be09
lovekushtyagi <lovekush@octifytechnologies.com>
2022-01-29 17:12:00 +0530

    put update comment module in post services index file

--- nextjs-frontend/pages/api/update-comment.ts
+++ nextjs-frontend/pages/api/update-comment.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { createNextJsRouteHandler } from '../../src/backend/utils/api-middle-ware/api-handler-factory';
 import { PerRequestContext } from '../../src/backend/utils/api-middle-ware/api-middleware-typings';
 import { beginTransactionMiddleWare, commitOrRollBackTransactionMiddleWare } from '../../src/backend/utils/api-middle-ware/transaction-middle-ware';
-import { updateComment } from '../../src/backend/services/posts-services/update-comment/update-comment';
+import { updateComment } from '../../src/backend/services/posts-services';
 import { authenticatedUserMiddleware } from '../../src/backend/utils/api-middle-ware/auth-middle-ware';
 
 
--- nextjs-frontend/src/backend/services/posts-services/index.ts
+++ nextjs-frontend/src/backend/services/posts-services/index.ts
@@ -2,6 +2,7 @@ import { createPost } from './create-post/create-post';
 import { likePost } from './like-post/like-post';
 import { createComment } from './create-comment/create-comment';
 import { updatePost } from './update-post/update-post';
+import { updateComment } from './update-comment/update-comment';
 import { unlikePost } from './unlike-post/unlike-post';
 
 export {
@@ -9,5 +10,6 @@ export {
     likePost,
     createComment,
     updatePost,
+    updateComment,
     unlikePost
 }
