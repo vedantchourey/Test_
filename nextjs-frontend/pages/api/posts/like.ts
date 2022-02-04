f3d57fef733f1d1b404e71f94e6e20d57440bb7a
piggy <babymechanic@gmail.com>
2022-02-04 18:26:09 +1100

    moving apis into folder for better namespacing

---
+++ nextjs-frontend/pages/api/posts/like.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createNextJsRouteHandler } from '../../../src/backend/utils/api-middle-ware/api-handler-factory';
+import { PerRequestContext } from '../../../src/backend/utils/api-middle-ware/api-middleware-typings';
+import { beginTransactionMiddleWare, commitOrRollBackTransactionMiddleWare } from '../../../src/backend/utils/api-middle-ware/transaction-middle-ware';
+import { likePost } from '../../../src/backend/services/posts-services';
+import { authenticatedUserMiddleware } from '../../../src/backend/utils/api-middle-ware/auth-middle-ware';
+
+
+export default createNextJsRouteHandler({
+  post: {
+    handler: async (req: NextApiRequest, res: NextApiResponse, context: PerRequestContext) => {
+      const result = await likePost(req.body, context);
+      res.status(result?.errors ? 400 : 200).send(result)
+    },
+    preHooks: [authenticatedUserMiddleware, beginTransactionMiddleWare],
+    postHooks: [commitOrRollBackTransactionMiddleWare]
+  }
+})
---
+++ nextjs-frontend/pages/api/posts/update.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createNextJsRouteHandler } from '../../../src/backend/utils/api-middle-ware/api-handler-factory';
+import { PerRequestContext } from '../../../src/backend/utils/api-middle-ware/api-middleware-typings';
+import { beginTransactionMiddleWare, commitOrRollBackTransactionMiddleWare } from '../../../src/backend/utils/api-middle-ware/transaction-middle-ware';
+import { updatePost } from '../../../src/backend/services/posts-services';
+import { authenticatedUserMiddleware } from '../../../src/backend/utils/api-middle-ware/auth-middle-ware';
+
+
+export default createNextJsRouteHandler({
+  patch: {
+    handler: async (req: NextApiRequest, res: NextApiResponse, context: PerRequestContext) => {
+      const result = await updatePost(req.body, context);
+      res.status(result.errors ? 400 : 200).send(result);
+    },
+    preHooks: [authenticatedUserMiddleware, beginTransactionMiddleWare],
+    postHooks: [commitOrRollBackTransactionMiddleWare]
+  }
+})
