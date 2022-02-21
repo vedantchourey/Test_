fad7ad09a5f3cdae896274c9a2eb6e6c65ec5a68
sandeep <sandeep@octifytechnologies.com>
2022-02-21 13:45:30 +0530

    fixed other profile card

--- nextjs-frontend/src/frontend/components/cards/others-profile-card/other-profile-card.tsx
+++ nextjs-frontend/src/frontend/components/cards/others-profile-card/other-profile-card.tsx
@@ -7,41 +7,46 @@ import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 const OtherProfileCard = (): unknown => {
   return (
     <Box className={styles.otherProfileCard}>
-      <Box className={styles.profileSection}>
-        <Box sx={{ position: 'relative' }}>
-          <Avatar sx={{ width: 85, height: 85, marginBottom: 2 }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          <IconButton className={styles.selectImg} >
-            {/* <img src='icons/gallery.svg' alt='icon' /> */}
-            <CollectionsIcon />
-          </IconButton>
-        </Box>
-        <Typography variant='h2' fontSize={24} my={2} sx={{ textTransform: 'capitalize', fontWeight: 600 }}>
-          Zain Vaccaro
-        </Typography>
-        <Typography variant='h3' fontSize={18} color='#695B6E' >
-          Westheimer Rd. Santa Ana, Illinois
-        </Typography>
-        <Box sx={{ width: "100%", my: 4 }}>
-          <Grid container spacing={3} >
-            <Grid item md={5} sx={{ alignSelf: 'center' }}>
-              <Divider />
-            </Grid>
-            <Grid item md={2}>
-              <Typography variant='h3' fontSize={18} >
-                About
-              </Typography>
-            </Grid>
-            <Grid item md={5} sx={{ alignSelf: 'center' }}>
-              <Divider />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box>
-          <Typography variant='body2' fontSize={13} sx={{ textAlign: 'center' }} >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesen
+      <Box className={styles.background}>
+        <Box className={styles.profileSection}>
+          <Box sx={{ position: 'relative' }}>
+            <Avatar sx={{ width: 85, height: 85, marginBottom: 2 }} alt="Remy Sharp" src="https://imagekit.io/blog/content/images/2019/12/image-optimization.jpg" />
+            <IconButton className={styles.selectImg} >
+              {/* <img src='icons/gallery.svg' alt='icon' /> */}
+              <CollectionsIcon />
+            </IconButton>
+          </Box>
+          <Typography variant='h2' fontSize={24} my={2} sx={{ textTransform: 'capitalize', fontWeight: 600 }}>
+            Zain Vaccaro
           </Typography>
+          <Typography variant='h3' fontSize={18} color='#695B6E' >
+            Westheimer Rd. Santa Ana, Illinois
+          </Typography>
+          <Box sx={{ width: "100%", my: 4 }}>
+            <Grid container spacing={3} >
+              <Grid item md={5} sx={{ alignSelf: 'center' }}>
+                <Divider />
+              </Grid>
+              <Grid item md={2}>
+                <Typography variant='h3' fontSize={18} >
+                  About
+                </Typography>
+              </Grid>
+              <Grid item md={5} sx={{ alignSelf: 'center' }}>
+                <Divider />
+              </Grid>
+            </Grid>
+          </Box>
+          <Box>
+            <Typography variant='body2' fontSize={13} sx={{ textAlign: 'center' }} >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesen
+            </Typography>
+          </Box>
         </Box>
-        <Box mt={5} sx={{ width: "100%" }}>
+      </Box>
+
+      <Box className={styles.bottom}>
+        <Box sx={{ width: "100%" }}>
           <Divider />
           <Grid container p={2}>
             <Grid item md={5} sx={{ textAlign: 'left' }}>
@@ -67,9 +72,9 @@ const OtherProfileCard = (): unknown => {
               </Typography>
             </Grid>
           </Grid>
-          <Divider />
 
-          <Grid container p={2}>
+          <Divider sx={{ mb: 3 }} />
+          <Grid container>
             <Grid item md={3} sx={{ textAlign: 'center' }}>
               <Typography variant='caption' fontSize={12}>
                 Followers
@@ -102,18 +107,17 @@ const OtherProfileCard = (): unknown => {
             </Grid>
           </Grid>
         </Box>
-        <Box sx={{ display: "flex", flexDirection: 'column', mt: 8 }}>
-          <Button startIcon={<PersonAddAltIcon />} sx={{
-            background: '#6931F9', padding: "8px 64px", mb: 2
-          }} variant='contained'>
+
+        <Box className={styles.btnSection}>
+          <Button className={styles.bottomBtn} startIcon={<PersonAddAltIcon />} variant='contained'>
             Follow
           </Button>
-          <Button sx={{ padding: "8px 64px" }} variant='outlined'>
+          <Button className={styles.bottomBtn} variant='outlined'>
             Message
           </Button>
         </Box>
       </Box>
-    </Box>
+    </Box >
   )
 }
 export default OtherProfileCard;
