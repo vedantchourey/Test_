e566bd41d4b63099c285b0b2cc14c2ca1c544ee9
lovekush <lovekush@octifytechnologies.com>
2022-02-28 19:20:14 +0530

    added some migrations policies

---
+++ db-migrations/migrations/sqls/20220228133634-addedPostLikesCommentsPolicies-down.sql
@@ -0,0 +1,3 @@
+DROP POLICY IF EXISTS authenticated_read_posts_table on posts;
+DROP POLICY IF EXISTS authenticated_read_post_likes_table on post_likes;
+DROP POLICY IF EXISTS authenticated_read_post_comments_table on post_comments;
