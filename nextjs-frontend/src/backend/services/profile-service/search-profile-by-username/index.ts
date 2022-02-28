f679da43fd5c4cac9b333168acb063e7fd40386c
lovekush <lovekush@octifytechnologies.com>
2022-02-28 13:26:18 +0530

    added search by username API

---
+++ nextjs-frontend/src/backend/services/profile-service/search-profile-by-username/index.ts
@@ -0,0 +1,22 @@
+import { PerRequestContext } from '../../../utils/api-middle-ware/api-middleware-typings';
+import { Knex } from 'knex';
+import { ServiceResponse } from '../../common/contracts/service-response';
+import { ProfilesRepository } from '../../database/repositories/profiles-repository';
+
+interface ISearchProfile {
+  id: string,
+  username:string,
+  avatarUrl: string
+}
+
+export async function searchUserByUsername(context: PerRequestContext): Promise<ServiceResponse<null, ISearchProfile>>{
+  const repository = new ProfilesRepository(context.transaction as Knex.Transaction);
+  const userName = context.getParamValue('username');
+  const profile = await repository.getProfileByUsername(userName as string);
+  const { id, username, avatarUrl } = profile;
+  return {data: {
+    id,
+    username,
+    avatarUrl
+  }}
+}
