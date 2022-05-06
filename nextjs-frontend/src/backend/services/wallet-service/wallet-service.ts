667dbd0343b261945548d3ba0b0306f7cfc5a3f6
prasads-meesho <prasadsaitwal@ip-192-168-0-152.ap-southeast-1.compute.internal>
2022-05-06 13:11:25 +0530

    integration razor pay

--- nextjs-frontend/src/backend/services/wallet-service/wallet-service.ts
+++ nextjs-frontend/src/backend/services/wallet-service/wallet-service.ts
@@ -9,44 +9,48 @@ import { validateWallet } from "./i-wallet-validator";
 
 export const creditBalance = async (
   req: IWalletRequest,
-  context: PerRequestContext
-): Promise<IWalletResponse | undefined> => {
+  connection: Knex.Transaction,
+  data: any = {}
+): Promise<IWalletResponse | undefined> => {  
   try {
     const errors = await validateWallet(req);
     if (errors) return { errors };
-    const user = await fetchUserById(req.userId, context.knexConnection as any);
+    
+    const user = await fetchUserById(req.userId, connection as any);
     if (!user) {
       return { errors: ["Invalid User Id"] };
     }
     const walletRepo = new WalletRepository(
-      context.transaction as Knex.Transaction
+      connection as Knex.Transaction
     );
     const wallet = await walletRepo.findByUserId(user.id);
     if (!wallet) {
       return { errors: ["Wallet not found"] };
     }
-    const data = await createTransaction(
-      { ...req, wallet_id: wallet.id },
-      context
+    const trans_resp = await createTransaction(
+      { ...req, wallet_id: wallet.id } as any,
+      connection,
+      data
     );
-    wallet.last_transaction_id = data?.id;
+    wallet.last_transaction_id = trans_resp?.id;
     wallet.balance = wallet.balance
       ? Number(wallet.balance) + Number(req.amount)
       : req.amount;
     const updated_data = await walletRepo.update(wallet, { id: wallet.id });
     return {
       ...updated_data,
-      transaction_id: data?.id,
+      transaction_id: trans_resp?.id,
     };
-  } catch (ex) {
-    if (context?.transaction) context?.transaction.rollback();
+  } catch (ex) {    
+    if (connection?.rollback) connection?.rollback();
     return { errors: ["Something went wrong"] };
   }
 };
 
 export const debitBalance = async (
   req: IWalletRequest,
-  context: PerRequestContext
+  context: PerRequestContext,
+  data: any = {}
 ): Promise<IWalletResponse | undefined> => {
   try {
     const errors = await validateWallet(req);
@@ -67,18 +71,19 @@ export const debitBalance = async (
         errors: ["Insufficient balance"],
       };
     }
-    const data = await createTransaction(
+    const trans_resp = await createTransaction(
       { ...req, wallet_id: wallet.id },
-      context
+      context.transaction as Knex.Transaction,
+      data
     );
-    wallet.last_transaction_id = data?.id;
+    wallet.last_transaction_id = trans_resp?.id;
     wallet.balance = wallet.balance
       ? Number(wallet.balance) - Number(req.amount)
       : req.amount;
     const updated_data = await walletRepo.update(wallet, { id: wallet.id });
     return {
       ...updated_data,
-      transaction_id: data?.id,
+      transaction_id: trans_resp?.id,
     };
   } catch (ex) {
     if (context?.transaction) context?.transaction.rollback();
