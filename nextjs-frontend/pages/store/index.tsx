238d2a6760097ccb280b3e65b246354e374c9ce3
Brijesh <brijeshraval227@gmail.com>
2022-05-13 19:05:24 +0530

    Store page side bar & responsive

--- nextjs-frontend/pages/store/index.tsx
+++ nextjs-frontend/pages/store/index.tsx
@@ -1,24 +1,104 @@
 import { Fragment } from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Box, Typography, Button, TextField, Checkbox, FormControlLabel } from '@mui/material';
 import Heading from '../../src/frontend/components/ui-components/typography/heading';
 import NoobPage from '../../src/frontend/components/page/noob-page';
 import Store from '../../src/frontend/components/store/store';
+import styles from "./store-sidebar.module.css";
+import SearchIcon from '@mui/icons-material/Search';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useAppSelector } from '../../src/frontend/redux-store/redux-store';
+import { isDeviceTypeSelector } from "../../src/frontend/redux-store/layout/layout-selectors";
+import { deviceTypes } from '../../src/frontend/redux-store/layout/device-types';
 
 const StoreIndex = (): JSX.Element => {
+
+  const isDesktop = useAppSelector((x) => isDeviceTypeSelector(x, deviceTypes.desktop));
 
   return (
     <NoobPage title="Store" metaData={{ description: "Noob Storm Store page" }}>
       <Fragment>
         <Container maxWidth="xl">
           <Heading divider heading={"STORE"} />
-          <Grid container spacing={2}>
+          <Grid container>
             <Grid item xs={12} lg={3}>
+            
+              {isDesktop && <div className={styles.sidebarContainer}>
+                <Typography className={styles.text}>Categories</Typography>
+                <Button variant="text" className={styles.button}>Headsets</Button>
+                <Button variant="text" className={styles.button}>Mice</Button>
+                <Button variant="text" className={styles.button}>PC Components</Button>
+                <Button variant="text" className={styles.button}>Keyboards</Button>
+                <Button variant="text" className={styles.button}>Monitors</Button>
+                <Button variant="text" className={styles.button}>Networking</Button>
+                <Button variant="text" className={styles.button}>Gaming Accessories</Button>
+                <Button variant="text" className={styles.button}>Laptops</Button>
+                <Box style={{ display: 'flex' }}>
+                  <span className={styles.border}></span>
+                </Box>              
+                <Typography className={styles.text}>Platform</Typography>
+                <Box className={styles.searchBar}>
+                  <TextField style={{ marginTop: '15px' }} placeholder="Search" variant="standard" InputProps={{ disableUnderline: true }} />
+                  <SearchIcon color="primary" />
+                </Box>
+                <div className={styles.inputRow}>
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>IPad</Typography>} />
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>Nintendo switch</Typography>}/>
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PC</Typography>}/>
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PlayStation 4</Typography>}/>
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PlayStation 5</Typography>}/>
+                  <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>Xbox one</Typography>}/>
+                </div>
+              </div>}
+
+              {!isDesktop && <Accordion className={styles.mainAccordion}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography className={styles.text}>Categories</Typography>
+                </AccordionSummary>
+                <AccordionDetails className={styles.accordionDetail}>
+                  <Button variant="text" className={styles.button}>Headsets</Button>
+                  <Button variant="text" className={styles.button}>Mice</Button>
+                  <Button variant="text" className={styles.button}>PC Components</Button>
+                  <Button variant="text" className={styles.button}>Keyboards</Button>
+                  <Button variant="text" className={styles.button}>Monitors</Button>
+                  <Button variant="text" className={styles.button}>Networking</Button>
+                  <Button variant="text" className={styles.button}>Gaming Accessories</Button>
+                  <Button variant="text" className={styles.button}>Laptops</Button>
+                  <Box style={{ display: 'flex' }}>
+                    <span className={styles.border}></span>
+                  </Box>              
+                  <Typography className={styles.text}>Platform</Typography>
+                  <Box className={styles.searchBar}>
+                    <TextField style={{ marginTop: '15px' }} placeholder="Search" variant="standard" InputProps={{ disableUnderline: true }} />
+                    <SearchIcon color="primary" />
+                  </Box>
+                  <div className={styles.inputRow}>
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>IPad</Typography>} />
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>Nintendo switch</Typography>}/>
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PC</Typography>}/>
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PlayStation 4</Typography>}/>
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>PlayStation 5</Typography>}/>
+                    <FormControlLabel control={<Checkbox />} label={<Typography className={styles.button}>Xbox one</Typography>}/>
+                  </div>
+                </AccordionDetails>
+              </Accordion>}
             </Grid>
-            <Grid item xs={12} lg={9} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-							<Store />
-							<Store />
-							<Store />
-            </Grid>
+            {isDesktop && 
+              <Grid item xs={12} lg={9} className={styles.containerDesktop}>
+                <Store />
+                <Store />
+                <Store />
+              </Grid>
+            }
+            {!isDesktop && 
+              <Grid item xs={12} lg={9} className={styles.containerMobile}>
+                <Store />
+                <Store />
+                <Store />
+              </Grid>
+            }
           </Grid>
         </Container>
       </Fragment>
--- nextjs-frontend/src/frontend/components/store/store.tsx
+++ nextjs-frontend/src/frontend/components/store/store.tsx
@@ -7,15 +7,10 @@ export default function Store(): JSX.Element {
   return (
     <Box className={styles.container}>
       <img src="/images/card.gif" className={commonStyles.fillImage} style={{ height: '230px' }}/>
-      
       <Typography className={styles.text} style={{ color: '#FFFFFF'}}>250 NoobStorm Credits</Typography>
-      
       <Typography className={styles.text} style={{ color: 'rgba(255, 255, 255, 0.4)' }}>Use credits for join tournaments and play with other games.</Typography>
-      
       <Typography className={styles.text} style={{ color: '#6932F9' }}>$9.99</Typography>
-
-      <Button variant="text" className={styles.button1}>Buy Now</Button>
-      
+      <Button variant="text" className={styles.button1}>Buy Now</Button>     
       <Button variant="text" className={styles.button2}><img src="/icons/Vector-Cart.png" style={{ width: '22px' }} /></Button>
     </Box>
   );
