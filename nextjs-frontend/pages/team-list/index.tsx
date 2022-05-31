957c567fdc125815a7c277cc1a7ccc11e66fab2a
himani154 <hmshah154@gmail.com>
2022-05-31 15:16:24 +0530

    add team in page

---
+++ nextjs-frontend/pages/team-list/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TeamList from "../../src/frontend/components/teamlist";
+
+const TeamListPage: React.FC<void> = () => {
+  return (
+    <TeamList key={"teamlist"}></TeamList>
+  );
+};
+
+export default TeamListPage;
