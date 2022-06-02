fd6001ce53af7eacdc2dd2f0eb8cb9e80519d585
Neel Patel <neelpatel.6531@gmail.com>
2022-06-02 11:09:36 +0530

    add team navigation

--- nextjs-frontend/src/frontend/components/teamlist/list.tsx
+++ nextjs-frontend/src/frontend/components/teamlist/list.tsx
@@ -7,61 +7,64 @@ import {
   TableCell,
   TableContainer,
   TableRow,
-  Typography
+  Typography,
 } from "@mui/material";
 import axios from "axios";
+import { useRouter } from "next/router";
 import React from "react";
 import { getAuthHeader } from "../../utils/headers";
 
 export const HeadCell = styled(TableCell)(() => ({
   borderTop: "1px solid #ffffff1a",
-  borderBottom: "none"
+  borderBottom: "none",
 }));
 
 export const NoobCell = styled(TableCell)(() => ({
   border: "1px solid #ffffff1a",
-  alignItems: "center"
+  alignItems: "center",
 }));
 
 export const NoobRow = styled(TableRow)(() => ({
-  align: "center"
+  align: "center",
 }));
 
 export interface Player {
   balance: number;
   firstName: string;
   lastName: string;
-  user_id: string
+  user_id: string;
 }
 
 export interface TeamData {
-  id: string
+  id: string;
   name: string;
-  players: Player[]
+  players: Player[];
 }
 
 const TeamListData: React.FC = () => {
-
   const [teamdata, setData] = React.useState<TeamData[]>([]);
-
-  const teamList = async () => {
-
+  const router = useRouter();
+  const teamList = async (): Promise<void> => {
     try {
-      const endpoint = 'api/teams';
+      const endpoint = "api/teams";
       const headers = await getAuthHeader();
       axios.get(endpoint, { headers: headers }).then((res) => {
         setData(res.data.result);
       });
     } catch (err) {
+      alert(err);
     }
+  };
+
+  const handleNavigation = async (id: string): Promise<void> => {
+    router.push(`/team/view/${id}/members`);
   };
 
   React.useEffect(() => {
     teamList();
-  }, [])
+  }, []);
 
   return (
-
     <React.Fragment>
       <Grid container rowSpacing={2} mt={4}>
         <Grid item xs={12} sm={12} md={12} mb={12}>
@@ -70,12 +73,27 @@ const TeamListData: React.FC = () => {
               <TableBody>
                 {teamdata.map((item) => {
                   return (
-                    <NoobRow sx={{ display: { sm: "flex", xs: "flex", md: "table-row" }, flexDirection: { sm: "column", xs: "column" } }} key={item.name}>
+                    <NoobRow
+                      sx={{
+                        display: { sm: "flex", xs: "flex", md: "table-row" },
+                        flexDirection: { sm: "column", xs: "column" },
+                      }}
+                      key={item.name}
+                      onClick={(): void => {
+                        handleNavigation(item.id);
+                      }}
+                    >
                       <NoobCell>
                         <Box alignItems="center" display="center">
-                          <img src="/icons/Rectangle.svg" width={"65px"} height={"65px"} />
+                          <img
+                            src="/icons/Rectangle.svg"
+                            width={"65px"}
+                            height={"65px"}
+                          />
                           <Box>
-                            <Typography marginLeft={2} marginRight={2}>{item.name}</Typography>
+                            <Typography marginLeft={2} marginRight={2}>
+                              {item.name}
+                            </Typography>
                             {/* <Typography marginLeft={2} marginRight={2}>{item.id}</Typography> */}
                           </Box>
                         </Box>
