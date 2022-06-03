7b0765dc3a6edd99a34c4a68886283322dab526f
prasads-meesho <prasadsaitwal@T9Y2JVV44Q.local>
2022-06-03 13:27:19 +0530

    added dropdown and filter

--- nextjs-frontend/src/frontend/components/free-agency-market/members/member/index1.tsx
+++ nextjs-frontend/src/frontend/components/free-agency-market/members/member/index1.tsx
@@ -1,44 +1,83 @@
 import styled from "@emotion/styled";
-import { Box, Button, MenuItem, Select, useMediaQuery, useTheme } from "@mui/material";
-
+import { Box, Button, useMediaQuery, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
+import GameDropDown from "../../../drop-downs/game-drop-down";
+import PlatformDropDown from "../../../drop-downs/platform-drop-down";
+import styles from "../../../tournaments/tournament-details-form.module.css";
 export const NoobButton = styled(Button)(() => ({
   color: "white",
   borderRadius: 0,
   textTransform: "capitalize",
 }));
 
-const MemberButton: React.FC = () => {
+const MemberButton = ({ setParam }: any): JSX.Element => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-
+  const [gameId, setGameId] = useState<string | undefined>("");
+  const [platformId, setPlatformId] = useState<string | undefined>("");
+  useEffect(() => {
+    // setParam
+    if (gameId || platformId) {
+      setParam({
+        gameId,
+        platformId,
+      });
+    }
+  }, [gameId, platformId]);
   return (
-    <>{isMobile ? (
-      <>
-        <Box sx={{ m: 1 }}>
-
-          <Select value={"search"} fullWidth={true}>
-            <MenuItem value="search">Search by Games</MenuItem>
-          </Select>
-        </Box>
-        <Box sx={{ m: 1 }}>
-          <Select value={"platform"} fullWidth={true}>
-            <MenuItem value="platform">Platform</MenuItem>
-          </Select>
-        </Box>
-      </>
-    ) : (
-      <>
-        <Box>
-          <Select value={"search"} sx={{ m: 1 }}>
-            <MenuItem value="search">Search by Games</MenuItem>
-          </Select>
-          <Select value={"platform"} sx={{ m: 1 }}>
-            <MenuItem value="platform">Platform</MenuItem>
-          </Select>
-        </Box>
-      </>
-    )
-    };
+    <>
+      {isMobile ? (
+        <>
+          <Box sx={{ m: 1 }}>
+            <GameDropDown
+              label="Game"
+              placeholder="Search by  Games"
+              onChange={(d: string | undefined): void => setGameId(d)}
+              value={gameId}
+              inputClassName={styles.inputItem}
+              autoCompleteClassName={styles.inputItem}
+            />
+          </Box>
+          <Box sx={{ m: 1 }}>
+            <PlatformDropDown
+              label="Platform"
+              placeholder="Select Platform"
+              allowAll
+              onChange={(id: any): void => setPlatformId(id)}
+              inputClassName={styles.inputItem}
+              autoCompleteClassName={styles.inputItem}
+              value={platformId}
+            />
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box width={500} display="flex">
+            <Box sx={{ mr: 2, width: "100%" }}>
+              <GameDropDown
+                label="Game"
+                placeholder="Search by  Games"
+                onChange={(d: string | undefined): void => setGameId(d)}
+                value={gameId}
+                inputClassName={styles.inputItem}
+                autoCompleteClassName={styles.inputItem}
+              />
+            </Box>
+            <Box sx={{ width: "100%" }}>
+              <PlatformDropDown
+                label="Platform"
+                placeholder="Select Platform"
+                allowAll
+                onChange={(id: any): void => setPlatformId(id)}
+                inputClassName={styles.inputItem}
+                autoCompleteClassName={styles.inputItem}
+                value={platformId}
+              />
+            </Box>
+          </Box>
+        </>
+      )}
+      ;
     </>
   );
 };
