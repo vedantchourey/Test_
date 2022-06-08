1b74ceea781039bddaaf563411a0ffb7ab17dd79
Neel Patel <neelpatel.6531@gmail.com>
2022-06-08 10:54:02 +0530

    add notification api

--- nextjs-frontend/src/frontend/components/header/notification-popover.tsx
+++ nextjs-frontend/src/frontend/components/header/notification-popover.tsx
@@ -1,5 +1,14 @@
-import { Button, Divider, List, ListItem, ListItemAvatar, ListItemText, ListSubheader, Typography } from '@mui/material';
-import * as React from 'react';
+import {
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from "@mui/material";
+import * as React from "react";
 
 interface Message {
   image: string;
@@ -7,48 +16,65 @@ interface Message {
 }
 
 interface Props {
-  message: Message[];
+  message: Message;
   onAccept: () => void;
   onDecline: () => void;
+  isActionRequired: boolean;
 }
 
 export default function BasicPopover(props: Props) {
   const { message, onAccept, onDecline } = props;
   return (
     <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
       subheader={
-        <ListSubheader sx={{ fontStyle: "norma", fontSize: "20px", fontWeight: 700, fontFamily: 'Inter', color: '#FFFFFF' }}>
+        <ListSubheader
+          sx={{
+            fontStyle: "norma",
+            fontSize: "20px",
+            fontWeight: 700,
+            fontFamily: "Inter",
+            color: "#FFFFFF",
+          }}
+        >
           Notifications
-        </ListSubheader >
+        </ListSubheader>
       }
     >
       <Divider variant="middle" component="li" />
-      < ListItem alignItems="flex-start" >
+      <ListItem alignItems="flex-start">
         <ListItemAvatar>
-          <img alt="Travis Howard" src={message[0].image} />
+          <img alt="Travis Howard" src={message.image} />
         </ListItemAvatar>
         <ListItemText
           secondary={
             <React.Fragment>
               <Typography
-                sx={{ display: 'inline' }}
+                sx={{ display: "inline" }}
                 component="span"
                 variant="body2"
                 color="text.primary"
               >
-                {message[0].text}
+                {message.text}
               </Typography>
             </React.Fragment>
           }
         />
-      </ListItem >
+      </ListItem>
       <ListItem sx={{ justifyContent: "left" }}>
         <ListItemAvatar />
-        <Button variant="contained" onClick={onAccept} sx={{ mr: 1 }}>Accept</Button>
-        <Button variant="outlined" onClick={onDecline} sx={{ mr: 1 }}>Decline</Button>
+        {props.isActionRequired && (
+          <>
+            <Button variant="contained" onClick={onAccept} sx={{ mr: 1 }}>
+              Accept
+            </Button>
+            <Button variant="outlined" onClick={onDecline} sx={{ mr: 1 }}>
+              Decline
+            </Button>
+          </>
+        )}
       </ListItem>
       <Divider variant="middle" component="li" />
-    </List >
+    </List>
   );
 }
--- nextjs-frontend/src/frontend/components/header/sideheader.tsx
+++ nextjs-frontend/src/frontend/components/header/sideheader.tsx
@@ -1,88 +1,183 @@
-import { Box, Button, Grid, IconButton, InputBase, Popover, Typography } from "@mui/material";
-import { useRouter } from 'next/router';
-import * as React from 'react';
-import { isLoggedInSelector, userNameSelector } from '../../redux-store/authentication/authentication-selectors';
-import { useAppSelector } from '../../redux-store/redux-store';
-import { walletDetaislSelector } from '../../redux-store/wallet/wallet-selector';
-import style from './desktop-sidebar.module.css';
-import BasicPopover from './notification-popover';
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  InputBase,
+  Popover,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useRouter } from "next/router";
+import * as React from "react";
+import {
+  isLoggedInSelector,
+  userNameSelector,
+} from "../../redux-store/authentication/authentication-selectors";
+import { useAppSelector } from "../../redux-store/redux-store";
+import { walletDetaislSelector } from "../../redux-store/wallet/wallet-selector";
+import { frontendSupabase } from "../../services/supabase-frontend-service";
+import { getAuthHeader } from "../../utils/headers";
+import { data } from "../team/members";
+import style from "./desktop-sidebar.module.css";
+import BasicPopover from "./notification-popover";
 
 export default function SideHeader(): JSX.Element {
-    const isLoggedIn = useAppSelector(isLoggedInSelector);
-    const username = useAppSelector(userNameSelector);
-    const wallet = useAppSelector(walletDetaislSelector);
-    const router = useRouter();
+  const isLoggedIn = useAppSelector(isLoggedInSelector);
+  const username = useAppSelector(userNameSelector);
+  const wallet = useAppSelector(walletDetaislSelector);
+  const router = useRouter();
+  const [notifications, setNotifications] = React.useState<any>([]);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const fetchNotifications = async (): Promise<void> => {
+    const headers = await getAuthHeader();
+    axios
+      .get("/api/notifications", {
+        headers: { ...headers },
+      })
+      .then((res) => {
+          console.log('res?.data?.data -> ', res?.data?.data)
+        const notificatiosData = (res?.data?.data || [])?.map((i: any) => ({
+          id: i.id,
+          message:
+            i.type === "TOURNAMENT_INVITE"
+              ? {
+                  image: "/icons/notification-data-image.svg",
+                  text: "You have new tournament invitations",
+                }
+              : {
+                  image: "/icons/notification-data-image.svg",
+                  text: "New notifications",
+                },
+          data: i,
+          isActionRequired: i.is_action_required
+        }));
+        setNotifications(notificatiosData);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-    const message = [
+  const submitNotification = async (id: string ,response: string): Promise<void> => {
+    const headers = await getAuthHeader();
+    axios
+      .post(
+        "/api/notifications",
+        { id, response },
         {
-            image: '/icons/notification-data-image.svg',
-            text: '"Andar" has chosen you as the Loser for the ENDPOINTGG VS CEX ESPORTS [2] tournament.'
+          headers: { ...headers },
         }
-    ];
+      )
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setAnchorEl(null);
+        fetchNotifications();
+      });
+  };
 
-    function onAccept(): void { }
-    function onDecline(): void { }
+  React.useEffect(() => {
+    if (isLoggedIn) {
+      fetchNotifications();
+    } else {
+    }
+  }, [isLoggedIn]);
 
-    return (
-        <>
-            {isLoggedIn && (
-                <Grid container spacing={1}>
-                    <Grid item md={7}></Grid>
-                    <Grid item md={5} >
-                        <div className={style.container4} style={{ justifyContent: 'flex-end' }}>
-                            <Box style={{ border: "1px solid #6931F9" }} sx={{ borderRadius: '16px' }}>
-                                <InputBase size="small" placeholder="Search anything..." sx={{ p: 1 }} />
-                                <IconButton sx={{ mr: 1}}>
-                                    <img src="/icons/search-icon.png" />
-                                </IconButton>
-                            </Box>
-                            <IconButton onClick={handleClick} sx={{ mr: 1, ml: 1}}>
-                                <img src="/icons/notification-icon.svg" />
-                            </IconButton>
-                            <img src="/images/16276393842661.png" className={style.img3} alt="logo" style={{ height: "50px", width: "50px", }} />
-                            <Box >
-                                <Typography className={style.text1}>{username}</Typography>
-                                <Button
-                                    variant="text"
-                                    size="small"
-                                    className={style.button4}
-                                    onClick={(): any => router.push("/wallet/info")}
-                                    startIcon={<img src="/icons/Vector-Wallet.png" />}
-                                >
-                                    {wallet?.balance}
-                                </Button>
-                            </Box>
-                        </div>
-                    </Grid>
-                </Grid>
-            )}
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
+  console.log("notifications -> ", notifications);
+
+  
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return (
+    <>
+      {isLoggedIn && (
+        <Grid container spacing={1}>
+          <Grid item md={7}></Grid>
+          <Grid item md={5}>
+            <div
+              className={style.container4}
+              style={{ justifyContent: "flex-end" }}
             >
-                <BasicPopover message={message} onAccept={onAccept} onDecline={onDecline} />
-            </Popover>
-        </>
-    )
+              <Box
+                style={{ border: "1px solid #6931F9" }}
+                sx={{ borderRadius: "16px" }}
+              >
+                <InputBase
+                  size="small"
+                  placeholder="Search anything..."
+                  sx={{ p: 1 }}
+                />
+                <IconButton sx={{ mr: 1 }}>
+                  <img src="/icons/search-icon.png" />
+                </IconButton>
+              </Box>
+              <IconButton onClick={handleClick} sx={{ mr: 1, ml: 1 }}>
+                <img src="/icons/notification-icon.svg" />
+              </IconButton>
+              <img
+                src="/images/16276393842661.png"
+                className={style.img3}
+                alt="logo"
+                style={{ height: "50px", width: "50px" }}
+              />
+              <Box>
+                <Typography className={style.text1}>{username}</Typography>
+                <Button
+                  variant="text"
+                  size="small"
+                  className={style.button4}
+                  onClick={(): any => router.push("/wallet/info")}
+                  startIcon={<img src="/icons/Vector-Wallet.png" />}
+                >
+                  {wallet?.balance}
+                </Button>
+              </Box>
+            </div>
+          </Grid>
+        </Grid>
+      )}
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        {notifications.map((i: any, idx: number) => (
+          <BasicPopover
+            message={i.message}
+            onAccept={() => {
+                submitNotification(i.id, "ACCEPTED");
+            }}
+            onDecline={() => {
+                submitNotification(i.id, "REJECT");
+            }}
+            isActionRequired={i.isActionRequired}
+            key={idx}
+          />
+        ))}
+      </Popover>
+    </>
+  );
 }
