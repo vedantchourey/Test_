f4e9b6b5894be6495d10f0f224e3cca4db99a785
nikhil garg <nikhilgargcoder@gmail.com>
2022-06-11 15:12:14 +0530

    Created two more endpoints

--- nextjs-frontend/src/models/constants.ts
+++ nextjs-frontend/src/models/constants.ts
@@ -20,7 +20,8 @@ export const TABLE_NAMES = {
     FREE_AGENCY_MARKET: "free_agency_market",
     WATCHLIST: "watchlist",
     ELO_RATING: "elo_ratings",
-    ELO_RATING_HISTORY: "elo_ratings_history"
+    ELO_RATING_HISTORY: "elo_ratings_history",
+    PRODUCTS: "products"
 }
 
 export const STATUS = {
