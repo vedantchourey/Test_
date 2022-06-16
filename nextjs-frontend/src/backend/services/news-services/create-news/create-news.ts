bee3fbf581cffbbf4bfa91e555dd431ea6042541
pratik-ranpariya <pratikranpariya007@gmail.com>
2022-06-17 00:12:53 +0530

    create news and newslist endpoint

---
+++ nextjs-frontend/src/backend/services/news-services/create-news/create-news.ts
@@ -0,0 +1,36 @@
+import { ICreateNewsRequest, ICreateNewsResponse, INewsResponse } from './i-create-news';
+import { PerRequestContext } from '../../../utils/api-middle-ware/api-middleware-typings';
+import { NewsRepository } from '../../database/repositories/news-repository';
+import { validatePost } from './create-news-validator';
+import { isThereAnyError } from '../../../../common/utils/validation/validator';
+import { Knex } from 'knex';
+import { ServiceResponse } from '../../common/contracts/service-response';
+
+export async function createNews(req: ICreateNewsRequest, context: PerRequestContext): Promise<ServiceResponse<Partial<ICreateNewsRequest>, ICreateNewsResponse>> {
+  const errors = await validatePost(req);
+  if (isThereAnyError(errors)) return {errors: errors}
+  const repository = new NewsRepository(context.transaction as Knex.Transaction);
+
+    const newsId = await repository.createNews({
+        title: req.title,
+        subtitle: req.subtitle,
+        author: req.author,
+        image: req.image,
+        description: req.description
+    });
+
+    const createdPost = await repository.getNewsUsingId(newsId as string);
+    const {id, createdAt, updatedAt, title, subtitle, author, image, description} = createdPost as INewsResponse;
+    return {
+      data: {
+        id,
+        title,
+        author,
+        subtitle,
+        image,
+        description,
+        updatedAt: updatedAt?.toISOString() as string,
+        createdAt: createdAt?.toISOString() as string
+      } as unknown as ICreateNewsResponse
+    }
+}
