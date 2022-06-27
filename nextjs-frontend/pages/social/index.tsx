4abcd42f22874387ae89ced6f008aa0eb1c67fb5
Neel Patel <neelpatel.6531@gmail.com>
2022-06-27 14:01:51 +0530

    add social media

---
+++ nextjs-frontend/pages/social/index.tsx
@@ -0,0 +1,71 @@
+import AuthGuard from "../../src/frontend/components/auth/auth-guard";
+import { NoobUserRole } from "../../src/backend/utils/api-middle-ware/noob-user-role";
+import NotFound from "../../src/frontend/components/not-found/not-found";
+import NoobPage from "../../src/frontend/components/page/noob-page";
+import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import { fetchUserFollowingList } from "../../src/frontend/service-clients/profile-service-client";
+import { getPostsByUserId } from "../../src/frontend/service-clients/post-service-client";
+import { IPostsResponse } from "../../src/frontend/service-clients/messages/i-posts-response";
+import { userProfileSelector } from "../../src/frontend/redux-store/authentication/authentication-selectors";
+import { useAppSelector } from "../../src/frontend/redux-store/redux-store";
+import PostCard from "../../src/frontend/components/account/posts/post-card";
+import { Skeleton } from "@mui/material";
+import CreatePostInput from "../../src/frontend/components/account/posts/create-post-input";
+
+const requiredRoles: NoobUserRole[] = ["noob-admin"];
+
+export default function Create(): JSX.Element {
+  const [isFetchingPosts, setIsFetchingPosts] = useState(true);
+  const [posts, setPosts] = useState<IPostsResponse[]>([]);
+
+  const user = useAppSelector(userProfileSelector);
+
+  const fetchPosts = async (): Promise<void> => {
+    try {
+      setIsFetchingPosts(true);
+      const followers = await fetchUserFollowingList(user?.id || "");
+      const fetchPostsBatch = followers.map((i) =>
+        getPostsByUserId(i.follower.id));
+      const posts: IPostsResponse[] = [];
+      const followerPosts = await Promise.all(fetchPostsBatch);
+      followerPosts.map((p: any) => {
+        p.map((fp: IPostsResponse) => posts.push(fp));
+      });
+      setPosts(posts);
+    } finally {
+      setIsFetchingPosts(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const _renderPosts = (): JSX.Element | React.ReactNode => {
+    if (isFetchingPosts) {
+      return new Array(5).fill("")
+.map((data, i) => <Skeleton key={i} />);
+    }
+    const jsx = posts.map((postData) => {
+      return <PostCard key={postData.id} data={postData} row={true} />;
+    });
+    return jsx;
+  };
+
+  return (
+    <NoobPage title="Social" metaData={{ description: "Noob storm home page" }}>
+      <AuthGuard
+        requiredRoles={requiredRoles}
+        renderOnCheckFailure={(): JSX.Element => <NotFound />}
+      >
+        <Box display={"flex"} justifyContent={"center"}>
+          <Box width={700}>
+            <CreatePostInput setPosts={setPosts} />
+            {_renderPosts()}
+          </Box>
+        </Box>
+      </AuthGuard>
+    </NoobPage>
+  );
+}
