ddcda4dfebcd1249f3bde16b60cd4c83fe92e251
Neel Patel <neelpatel.6531@gmail.com>
2022-07-06 11:26:45 +0530

    add online offline logic

--- nextjs-frontend/src/frontend/components/view-tournament/index.tsx
+++ nextjs-frontend/src/frontend/components/view-tournament/index.tsx
@@ -509,10 +509,20 @@ const ViewTournament: React.FC = () => {
               justifyContent={"flex-end"}
             >
               <Box marginRight="16px">
-                <Typography>
-                  <span style={{ color: "#FF0000" }}> Round 1 begins in :</span>{" "}
-                  {countDown}
-                </Typography>
+                {countDown !== "00:00:00" ? (
+                  <Typography>
+                    <span style={{ color: "#FF0000" }}>
+                      Round 1 begins in :
+                    </span>{" "}
+                    {countDown}
+                  </Typography>
+                ) : (
+                  <Typography>
+                    <span style={{ color: "#FF0000" }}>
+                      Tournament already started
+                    </span>
+                  </Typography>
+                )}
               </Box>
 
               <Box marginRight="16px">
