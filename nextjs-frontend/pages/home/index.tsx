73f6848329078cc7636ce3ffefc6406bce488c16
vishantdhameliya <71579333+vishantdhameliya@users.noreply.github.com>
2022-07-27 17:25:48 +0530

    lint changes

--- nextjs-frontend/pages/home/index.tsx
+++ nextjs-frontend/pages/home/index.tsx
@@ -32,8 +32,7 @@ import axios from "axios";
 
 const Home = (): JSX.Element => {
   const isDesktop = useAppSelector((x) =>
-    isDeviceTypeSelector(x, deviceTypes.desktop)
-  );
+    isDeviceTypeSelector(x, deviceTypes.desktop));
   const [newsData, setNewsData] = useState<any[]>([]);
   const [posts, setPosts] = useState<IPostsResponse[]>([]);
   const [isFetchingPosts, setIsFetchingPosts] = useState<boolean>(true);
