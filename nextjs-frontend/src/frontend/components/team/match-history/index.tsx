e2b479fa1cdca96d8c9996a54230e0f6a5f62873
vishantdhameliya <71579333+vishantdhameliya@users.noreply.github.com>
2022-07-28 13:59:30 +0530

    team related changes in my team

--- nextjs-frontend/src/frontend/components/team/match-history/index.tsx
+++ nextjs-frontend/src/frontend/components/team/match-history/index.tsx
@@ -11,12 +11,9 @@ import {
   NoobTableRow,
 } from "../../ui-components/table/AltTable";
 import { IMatchHubData } from "../../../../../pages/match-hub";
-import { userProfileSelector } from "../../../redux-store/authentication/authentication-selectors";
-import { useAppSelector } from "../../../redux-store/redux-store";
 import { useRouter } from "next/router";
 
-const MatchHistory: React.FC<{ data: IMatchHubData[] }> = (props) => {
-  const user = useAppSelector(userProfileSelector);
+const MatchHistory: React.FC<{ data: IMatchHubData[], teamId: string }> = (props) => {
   const router=useRouter();
   
   return (
@@ -33,16 +30,6 @@ const MatchHistory: React.FC<{ data: IMatchHubData[] }> = (props) => {
             </NoobTableHead>
             <TableBody>
               {props.data.map((row, idx) => {
-              //   const opponent1Name = row.opponent1.user_id
-              //   ? `${row.opponent1.firstName} ${row.opponent1.lastName}`
-              //   : row.opponent1.name || "N/A";
-              // const opponent2Name = row.opponent2.user_id
-              //   ? `${row.opponent2.firstName} ${row.opponent2.lastName}`
-              //   : row.opponent2.name || "N/A";
-                const myPlayer =
-                row.opponent1.user_id === user?.id
-                  ? row.opponent1
-                  : row.opponent2;
                 return (
                   <NoobTableRow key={idx}>
                     <NoobTableCell
@@ -52,16 +39,10 @@ const MatchHistory: React.FC<{ data: IMatchHubData[] }> = (props) => {
                       }}
                       //onClick={():void=>{router.push(`/team/view/${row.opponent1.user_id === user?.id ? row.opponent1.team_id:row.opponent2.team_id}/members`)}}
                     >
-                      {/* <Typography>{opponent1Name}</Typography>
-                      <Typography color={"#F09633"}>VS</Typography> */}
-                      <Typography>{row.opponent1.user_id === user?.id ? row.opponent1.name:row.opponent2.name}</Typography>
+                      <Typography>{row.opponent1.team_id === props.teamId ? row.opponent2.name:row.opponent1.name}</Typography>
                     </NoobTableCell>
                     <NoobTableCell>
-                      {myPlayer.result
-                        ? myPlayer.result === "win"
-                          ? "Win"
-                          : "Loss"
-                        : "-"}
+                        {row.opponent1.team_id === props.teamId? (row.opponent2.result?row.opponent2.result:"-") : (row.opponent1.result?row.opponent1.result:"-")}
                     </NoobTableCell>
                     <NoobTableCell onClick={():void=>{router.push(`/view-tournament/${row.tournament_id}/details`)}}>
                       <Typography>{row.tournament_name}</Typography>
