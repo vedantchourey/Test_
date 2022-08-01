aa91866ed0eb915a569d83b91869dcb0e87cd43d
Neel Patel <neelpatel.6531@gmail.com>
2022-08-02 03:43:05 +0530

    fix label color in tickets

--- nextjs-frontend/src/frontend/components/support-tickets/index.tsx
+++ nextjs-frontend/src/frontend/components/support-tickets/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Chip, Grid, Typography } from "@mui/material";
+import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import { ReactComponent as GameIcon } from "../../../../public/icons/GameIcon.svg";
@@ -107,16 +107,12 @@ const SupportList: React.FC = () => {
     {
       title: "Status",
       renderCell: (row): any => {
-        let color = "#28D89C";
-        if (row.status === "DRAFTED") {
-          color = "#D52D3E";
-        }
         return (
           <Chip
             label={row.status}
+            color={row.status === "resolve" ? "success" : "warning"}
             style={{
               textTransform: "capitalize",
-              background: color,
               padding: "0px 10px",
             }}
           />
@@ -128,18 +124,13 @@ const SupportList: React.FC = () => {
       title: "Action",
       renderCell: (row): JSX.Element => {
         return (
-          <Chip
-            label={"Resolve"}
+          <Button
             onClick={(): void => {
-              onClickResolveTicket(row.id, row.status);
+              onClickResolveTicket(row.id, "resolve");
             }}
-            style={{
-              textTransform: "capitalize",
-              background: "#FBAF40",
-              padding: "0px 10px",
-              cursor: "pointer",
-            }}
-          />
+          >
+            Resolve
+          </Button>
         );
       },
       width: "10%",
