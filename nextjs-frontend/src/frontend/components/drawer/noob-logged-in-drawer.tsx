3f6689b29c73139a3d25c54d6e5cde102cfed0e1
milan vala <milanvala31@gmail.com>
2022-10-27 20:00:58 +0530

    mobile sidebar fix

--- nextjs-frontend/src/frontend/components/drawer/noob-logged-in-drawer.tsx
+++ nextjs-frontend/src/frontend/components/drawer/noob-logged-in-drawer.tsx
@@ -1,22 +1,34 @@
-import { Button, Dialog, Divider, IconButton, ListItemIcon, ListItemText, MenuItem, MenuList, useTheme } from '@mui/material';
-import styles from './noob-drawer.module.css';
-import * as React from 'react';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import FlashOnIcon from '@mui/icons-material/FlashOn';
-import DiscordIcon from '../icons/discord-icon';
-import TwitchIcon from '../icons/twitch-icon';
-import YoutubeIcon from '../icons/youtube-icon';
-import { useRouter } from 'next/router';
-import PersonIcon from '@mui/icons-material/Person';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import SettingsIcon from '@mui/icons-material/Settings';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import LogoutIcon from '@mui/icons-material/Logout';
-import CloseIcon from '@mui/icons-material/Close';
-import Image from 'next/image';
-import commonStyles from '../../styles/common.module.css';
+import {
+  Button,
+  Dialog,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import styles from "./noob-drawer.module.css";
+import * as React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import DiscordIcon from "../icons/discord-icon";
+import TwitchIcon from "../icons/twitch-icon";
+import YoutubeIcon from "../icons/youtube-icon";
+import { useRouter } from "next/router";
+import GroupIcon from '@mui/icons-material/Group';
+import HomeIcon from '@mui/icons-material/Home';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import EmailIcon from '@mui/icons-material/Email';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import ForumIcon from '@mui/icons-material/Forum';
+import LogoutIcon from "@mui/icons-material/Logout";
+import CloseIcon from "@mui/icons-material/Close";
+import Image from "next/image";
+import commonStyles from "../../styles/common.module.css";
 
 interface Props {
   show: boolean;
@@ -25,7 +37,7 @@ interface Props {
 }
 
 export default function NoobLoggedInDrawer(props: Props): JSX.Element {
-  const {show, onClose, onLogoutClick} = props;
+  const { show, onClose, onLogoutClick } = props;
   const theme = useTheme();
   const router = useRouter();
 
@@ -38,53 +50,106 @@ export default function NoobLoggedInDrawer(props: Props): JSX.Element {
     <Dialog open={show} onClose={onClose} fullScreen color="#08001C">
       <div className={styles.container}>
         <div className={commonStyles.flexGroup}>
-          <div className={commonStyles.flexEqualSpace}/>
+          <div className={commonStyles.flexEqualSpace} />
           <div className={commonStyles.flexEqualSpace}>
-            <Image src="/images/noobstorm-logo-small.png"
+            <Image
+              src="/images/noobstorm-logo-small.png"
               width={130}
               height={28}
-              alt="noob storm logo"/>
+              alt="noob storm logo"
+            />
           </div>
           <div className={styles.topMenuItemRight}>
             <IconButton onClick={onClose}>
-              <CloseIcon/>
+              <CloseIcon />
             </IconButton>
           </div>
         </div>
         <div className={styles.topMenuGroup}>
           <MenuList>
-            <MenuItem onClick={(): Promise<void> => gotoPage('/account')}><ListItemIcon><PersonIcon fontSize="small"/></ListItemIcon><ListItemText>Account</ListItemText></MenuItem>
-            <MenuItem><ListItemIcon><DashboardIcon fontSize="small"/></ListItemIcon><ListItemText>Dashboard</ListItemText></MenuItem>
-            <MenuItem><ListItemIcon><ShoppingBagIcon fontSize="small"/></ListItemIcon><ListItemText>Orders</ListItemText></MenuItem>
-            <MenuItem><ListItemIcon><AccountBalanceWalletIcon fontSize="small"/></ListItemIcon><ListItemText>Wallet</ListItemText></MenuItem>
-            <MenuItem><ListItemIcon><SettingsIcon fontSize="small"/></ListItemIcon><ListItemText>Profile Settings</ListItemText></MenuItem>
-            <MenuItem><ListItemIcon><WatchLaterIcon fontSize="small"/></ListItemIcon><ListItemText>Active Tournaments</ListItemText></MenuItem>
-            <Divider/>
-            <MenuItem onClick={onLogoutClick}><ListItemIcon><LogoutIcon fontSize="small"/></ListItemIcon><ListItemText>Logout</ListItemText></MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/")}>
+              <ListItemIcon>
+                <HomeIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/tournaments-list")}>
+              <ListItemIcon>
+                <EmojiEventsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Tournaments</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/leaderboard")}>
+              <ListItemIcon>
+                <AssessmentIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Leaderboards</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/chat")}>
+              <ListItemIcon>
+                <EmailIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Message</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/about-us")}>
+              <ListItemIcon>
+                <GroupIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>About Us</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/support")}>
+              <ListItemIcon>
+                <HelpOutlineIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Support</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={(): Promise<void> => gotoPage("/faq")}>
+              <ListItemIcon>
+                <ForumIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>FAQ</ListItemText>
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={onLogoutClick}>
+              <ListItemIcon>
+                <LogoutIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Logout</ListItemText>
+            </MenuItem>
           </MenuList>
         </div>
-        <Divider/>
-        <div className={styles.flexRowWrap}>
-          <MenuItem><ListItemText primary="Home" onClick={(): Promise<void> => gotoPage('/')}/></MenuItem>
-          <MenuItem><ListItemText primary="Leaderboards"/></MenuItem>
-          <MenuItem><ListItemText primary="About Us"/></MenuItem>
-          <MenuItem><ListItemText primary="Support"/></MenuItem>
-          <MenuItem><ListItemText primary="FAQ"/></MenuItem>
-        </div>
-        <Divider/>
+        <Divider />
         <div className={styles.bottomMenuGroup}>
-          <Button variant="text" startIcon={<ShoppingCartIcon/>} style={{textTransform: 'none'}}>
+          <Button
+            variant="text"
+            startIcon={<ShoppingCartIcon />}
+            style={{ textTransform: "none" }}
+          >
             Store
           </Button>
-          <Button variant="text" startIcon={<FlashOnIcon/>} style={{color: theme.palette.secondary.main, textTransform: 'none'}}>
+          <Button
+            variant="text"
+            startIcon={<FlashOnIcon />}
+            style={{
+              color: theme.palette.secondary.main,
+              textTransform: "none",
+            }}
+          >
             Free Agency Market
           </Button>
         </div>
         <div className={styles.footerContainer}>
           <div className={styles.footer}>
-            <DiscordIcon/>
-            <TwitchIcon/>
-            <YoutubeIcon/>
+            <DiscordIcon />
+            <TwitchIcon />
+            <YoutubeIcon />
+          </div>
+          <Divider />
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop: 5 }}>
+            <Typography sx={{ fontSize: 10 }}>
+              Copyright © 2022. All Rights Reserved By
+            </Typography>
+            <Typography sx={{ color: "#6931F9", fontSize: 10, marginLeft: "3px" }}>NOOBSTORM</Typography>
           </div>
         </div>
       </div>
