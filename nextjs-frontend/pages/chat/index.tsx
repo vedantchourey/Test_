bf978c7f13c3f4599f9291db85f51e4c4b33d19d
milan vala <milanvala31@gmail.com>
2022-10-31 16:54:48 +0530

    fix chat

--- nextjs-frontend/pages/chat/index.tsx
+++ nextjs-frontend/pages/chat/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Chat from "../../src/frontend/components/chat";
 import NoobPage from "../../src/frontend/components/page/noob-page";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 export default function ChatPage(): JSX.Element {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <NoobPage title="Chat" metaData={{}} hideChat={true}>
-      <>
+      <div style={isMobile ? { width: "85%", marginLeft: 20 } : {}}>
         <h1 style={{margin:"20px",color:"#FFFFFF", textAlign: "center"}}>Chat</h1>
-        <Chat smallChat={false} />
-      </>
+        <Chat smallChat={isMobile ? true : false} />
+      </div>
     </NoobPage>
   );
 }
