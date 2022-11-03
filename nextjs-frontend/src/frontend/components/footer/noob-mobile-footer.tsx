e0adaae1e884b05038687e53e967bbda52711009
milan vala <milanvala31@gmail.com>
2022-11-03 19:23:51 +0530

    fix footer

--- nextjs-frontend/src/frontend/components/footer/noob-mobile-footer.tsx
+++ nextjs-frontend/src/frontend/components/footer/noob-mobile-footer.tsx
@@ -6,6 +6,9 @@ import { useRouter } from 'next/router';
 import YoutubeIcon from '../icons/youtube-icon';
 import TwitchIcon from '../icons/twitch-icon';
 import DiscordIcon from '../icons/discord-icon';
+import { useAppSelector } from "../../../../src/frontend/redux-store/redux-store";
+import { isDeviceTypeSelector } from "../../../../src/frontend/redux-store/layout/layout-selectors";
+import { deviceTypes } from '../../../../src/frontend/redux-store/layout/device-types';
 
 export default function NoobMobileFooter(): JSX.Element {
   const theme = useTheme();
@@ -24,31 +27,64 @@ export default function NoobMobileFooter(): JSX.Element {
     await router.push('/')
   }
 
-  return (
+  const isDesktop = useAppSelector((x) => isDeviceTypeSelector(x, deviceTypes.desktop));
+
+  return isDesktop ? (
     <div className={styles.footerContainer}>
       <Grid container spacing={2}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Image src="/images/noobstorm-logo-small.png"
+            <Image
+              src="/images/noobstorm-logo-small.png"
               width={130}
               height={28}
-              alt="noob storm logo" />
+              alt="noob storm logo"
+            />
           </Grid>
           <Grid item xs={12}>
-            <Button variant="text" style={buttonStyle(['/tournaments'])}>Tournaments</Button>
-            <Button variant="text" style={buttonStyle(['/leaderboards'])}>Leaderboards</Button>
-            <Button variant="text" onClick={gotoAboutUsPage} style={buttonStyle(['/about-us'])}>About Us</Button>
-            <Button variant="text" style={buttonStyle(['/support'])}>Support</Button>
-            <Button variant="text" style={buttonStyle(['/support'])}>FAQ</Button>
+            <Button variant="text" style={buttonStyle(["/tournaments"])}>
+              Tournaments
+            </Button>
+            <Button variant="text" style={buttonStyle(["/leaderboards"])}>
+              Leaderboards
+            </Button>
+            <Button
+              variant="text"
+              onClick={gotoAboutUsPage}
+              style={buttonStyle(["/about-us"])}
+            >
+              About Us
+            </Button>
+            <Button variant="text" style={buttonStyle(["/support"])}>
+              Support
+            </Button>
+            <Button variant="text" style={buttonStyle(["/support"])}>
+              FAQ
+            </Button>
           </Grid>
           <Grid item xs={12}>
-            <IconButton size='medium' color="default" aria-label="Youtube" component="span">
+            <IconButton
+              size="medium"
+              color="default"
+              aria-label="Youtube"
+              component="span"
+            >
               <YoutubeIcon />
             </IconButton>
-            <IconButton size='medium' color="default" aria-label="Twitch" component="span">
+            <IconButton
+              size="medium"
+              color="default"
+              aria-label="Twitch"
+              component="span"
+            >
               <TwitchIcon />
             </IconButton>
-            <IconButton size='medium' color="default" aria-label="Discord" component="span">
+            <IconButton
+              size="medium"
+              color="default"
+              aria-label="Discord"
+              component="span"
+            >
               <DiscordIcon />
             </IconButton>
           </Grid>
@@ -57,13 +93,83 @@ export default function NoobMobileFooter(): JSX.Element {
           <Divider />
         </Grid>
         <Grid item xs={12}>
-          <Typography color={"default"} variant="h3">
-            Copyright © 2021. All Rights Reserved By <Button variant="text" onClick={gotoHomePage}>
+          <Typography color={"default"} variant="h3" fontSize={10}>
+            Copyright © 2022. All Rights Reserved By
+            <Button variant="text" onClick={gotoHomePage}>
               NOOBSTORM
             </Button>
           </Typography>
         </Grid>
       </Grid>
     </div>
-  )
+  ) : (
+    <div className={styles.mobileContainer}>
+      <Image
+        src="/images/noobstorm-logo-small.png"
+        width={130}
+        height={28}
+        alt="noob storm logo"
+      />
+      <Button variant="text" style={buttonStyle(["/tournaments"])}>
+        Tournaments
+      </Button>
+      <Button variant="text" style={buttonStyle(["/leaderboards"])}>
+        Leaderboards
+      </Button>
+      <Button
+        variant="text"
+        onClick={gotoAboutUsPage}
+        style={buttonStyle(["/about-us"])}
+      >
+        About Us
+      </Button>
+      <Button variant="text" style={buttonStyle(["/support"])}>
+        Support
+      </Button>
+      <Button variant="text" style={buttonStyle(["/support"])}>
+        FAQ
+      </Button>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <IconButton
+          size="medium"
+          color="default"
+          aria-label="Youtube"
+          component="span"
+        >
+          <YoutubeIcon />
+        </IconButton>
+        <IconButton
+          size="medium"
+          color="default"
+          aria-label="Twitch"
+          component="span"
+        >
+          <TwitchIcon />
+        </IconButton>
+        <IconButton
+          size="medium"
+          color="default"
+          aria-label="Discord"
+          component="span"
+        >
+          <DiscordIcon />
+        </IconButton>
+      </div>
+      <Divider style={{ width: "30%", marginTop: 10 }} />
+      <Typography
+        color={"default"}
+        variant="h3"
+        fontSize={12}
+        style={{ display: "flex", flexDirection: "row", marginTop: 10 }}
+      >
+        Copyright © 2022. All Rights Reserved By
+        <Typography
+          onClick={gotoHomePage}
+          style={{ fontSize: 12, color: "white", marginLeft: 3 }}
+        >
+          NOOBSTORM
+        </Typography>
+      </Typography>
+    </div>
+  );
 }
